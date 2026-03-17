7719a25ba341fde55eb43cd0a3c95446cd272060
cmayta <cesarmayta@gmail.com>
2026-03-16 21:51:04 -0500

    sentencias dml

---
+++ dia3/mysql/sentencias_dml.sql
@@ -0,0 +1,23 @@
+use db_matriculas;
+-- SENTENCIAS DML
+-- CRUD
+-- C - INSERT
+-- R - SELECT
+-- U - UPDATE
+-- D - DELETE
+
+-- INSERT
+insert into alumno(nro_documento,nombre) values('100','cesar');
+
+INSERT INTO alumno(nro_documento,nombre,nota)
+VALUES
+('200','ana',15),
+('300','luis',20),
+('400','jose',11),
+('500','raul',10),
+('600','carmen',13),
+('700','jorge',16),
+('800','daniel',20),
+('900','luisa',17),
+('1000','pedro',5);
+
