4c63df8b3ae400cde76c0487a4a629b0ba591ca7
cmayta <cesarmayta@gmail.com>
2026-03-16 22:11:41 -0500

    sentencias dml

--- dia3/mysql/sentencias_dml.sql
+++ dia3/mysql/sentencias_dml.sql
@@ -7,7 +7,7 @@ use db_matriculas;
 -- D - DELETE
 
 -- INSERT
-insert into alumno(nro_documento,nombre) values('100','cesar');
+insert into alumno(nro_documento,nombre) values('1000','oscar');
 
 INSERT INTO alumno(nro_documento,nombre,nota)
 VALUES
@@ -21,3 +21,21 @@ VALUES
 ('900','luisa',17),
 ('1000','pedro',5);
 
+-- SELECT READ O LEER DATOS
+select * from alumno;
+select nombre,nota from alumno;
+-- select con where
+select nombre,nota from alumno
+where nota > 10;
+select nombre,nota from alumno
+where nombre like '%l%';
+
+-- ACTUALIZAR Y ELIMINAR DATOS
+UPDATE alumno set email = 'codigo@gmail.com';
+-- ACTUALIZAR CON CONDICIONAL WHERE
+UPDATE alumno set email = 'cesar@gmail.com' where id = 1;
+-- ACTUALIZAR CON FUNCIONES
+UPDATE alumno set email = CONCAT(nombre,'@gmail.com');
+
+-- ELIMINAR
+DELETE from alumno where id = 10;
