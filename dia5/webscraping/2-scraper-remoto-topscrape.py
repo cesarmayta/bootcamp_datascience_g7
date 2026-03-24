f8d77e14fff7cadb488e10533db0ca7059f54081
cmayta <cesarmayta@gmail.com>
2026-03-23 21:36:41 -0500

    scrapper remoto terminado

--- dia5/webscraping/2-scraper-remoto-topscrape.py
+++ dia5/webscraping/2-scraper-remoto-topscrape.py
@@ -10,5 +10,13 @@ if response.status_code == 200:
     data = response.content
     soup = BeautifulSoup(response.content,'html.parser')
     books = soup.find_all('article',class_='product_pod')
-    print(books[0])
     print(f'total de libros : {len(books)}')
+    lista_libros = []
+    print(books[0])
+    for book in books:
+        title = book.find('h3').find('a')['title']
+        price = book.find('p',class_='price_color').get_text()
+        lista_libros.append([title,price])
+        
+    print(tabulate(lista_libros,headers=['Titulo','Precio'],tablefmt='grid'))
+        
