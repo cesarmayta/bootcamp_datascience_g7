c54b56c2fc63d8a90b91113cf26440979a3977cd
cmayta <cesarmayta@gmail.com>
2026-06-05 22:58:17 -0500

    app lista

--- dia2/housing-web/app.py
+++ dia2/housing-web/app.py
@@ -12,13 +12,14 @@ app = Flask(__name__)
 @app.route('/',methods=['GET','POST'])
 def index():
     precio = 0
+    rooms = 1
     if request.method == "POST":
         rooms = int(request.form['rooms'])
         rooms_sc = sc_x.transform(np.array([[rooms]]))
         prediction_sc = model.predict(rooms_sc)
         prediction = sc_y.inverse_transform(prediction_sc) * 10000
-        precio = round(prediction[0][0],2)
-    return render_template('index.html',precio=precio)
+        precio = abs(round(prediction[0][0],2))
+    return render_template('index.html',precio=precio,rooms=rooms)
 
 if __name__ == "__main__":
     app.run(debug=True)
