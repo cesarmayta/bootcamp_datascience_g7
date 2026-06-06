793930fdc328d828c4bf6177e9e1f51d6ef66fa1
cmayta <cesarmayta@gmail.com>
2026-06-05 20:11:37 -0500

    request

--- dia2/flask/primer_app.py
+++ dia2/flask/primer_app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 #creamos un objeto de clase Flask
 app = Flask(__name__)
@@ -6,5 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<center><h1>Hola mundo con Flask</h1></center>'
+
+@app.route('/saludo')
+def saludo():
+    nombre = request.args.get('nombre','')
+    return f"<h1>Hola {nombre}</h1>"
+
+@app.route('/sumar/<int:a>/<int:b>')
+def sumar(a,b):
+    resultado = a + b
+    return f"<center><h1>La suma de {a} + {b} es {resultado} </h1></center>"
 
 app.run(debug=True)
